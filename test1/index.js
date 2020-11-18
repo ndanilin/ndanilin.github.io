8743672b2d0e45ecb2c954cd4c240e331d3b47ef
ndanilin <ndanilin87@gmail.com>
2020-11-18 18:52:22 +0300

    добавил экспорт таблицы в CSV

--- test1/index.js
+++ test1/index.js
@@ -3,26 +3,8 @@ var source = document.getElementById("source");
 var rootForTable = document.getElementById("table-scroll");
 var list;
 
-function createPDF(){
-    var sTable = rootForTable.innerHTML;
-
-    if (document.querySelector("table") === null){
-        console.error("the table is empty. you cannot create pdf");
-    }
-
-    var style = "<style>";
-    style = style + "table {width: 100%;font: 17px Calibri;}";
-    style = style + "table, th, td {border: solid 1px #DDD; border-collapse: collapse;";
-    style = style + "padding: 2px 3px;text-align: center;}";
-    style = style + "</style>";
-
-    var win = window.open('', '', 'height=700,width=700');
-
-    win.document.write(style);
-    win.document.write(sTable);
-    win.document.close();
-
-    win.print();
+function exportCsv(){
+    $("table").first().table2csv();
 }
 
 function resetStyle() {
